32bceb1878e2e0660b06113e7a834df4543354c4
dannyroes <danny.roes@gmail.com>
2019-05-13 21:00:22 -0400

    v1.1

--- math.go
+++ math.go
@@ -1,7 +1,22 @@
 // Package badmath implements math functions for accounting purposes
 package badmath
 
-// Add returns the sum of two ints
-func Add(x, y int) int {
-	return x + y
+// Add returns the sum of two or more ints
+func Add(nums ...int) int {
+	total := 0
+	for _, x := range nums {
+		total += x
+	}
+
+	return total
+}
+
+// Multiply returns the product of x times y
+func Multiply(x, y int) int {
+	total := 0
+	for i := 0; i <= x; i++ {
+		total = Add(total, y)
+	}
+
+	return total
 }
--- readme.md
+++ readme.md
@@ -2,6 +2,9 @@
 A library for your important accounting calculations
 
 ## Changelog
+### 1.1.0
+* Add function now takes an arbitrary number of arguments
+* Multiply(x,y) returns the product of x times y
 ### 1.0.0
 Initial release
 * Add(x,y) returns the sum of two ints
